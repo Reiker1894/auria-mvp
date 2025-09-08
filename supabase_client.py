3842bc3ce1c95486470cb98fe3e1771f18c16f16
Jeffrey Steban Cruz <63203380+Reiker1894@users.noreply.github.com>
2025-09-07 19:02:51 -0500

    Create supabase_client.py

---
+++ supabase_client.py
@@ -0,0 +1,8 @@
+!pip install supabase
+from supabase import create_client, Client
+
+# Usa tus propios valores aquí:
+SUPABASE_URL = "https://trrxzgtfydvojvtrvpkh.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRycnh6Z3RmeWR2b2p2dHJ2cGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyODg0MzYsImV4cCI6MjA3Mjg2NDQzNn0.O3Xj1zAb4JuO4hbijzOyKZNudgME0IEtppb8EB7oFEc"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
